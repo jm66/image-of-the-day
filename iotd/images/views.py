8d063643c6f83c4acbc3f7ca46c696c1e0d781c1
JM Lopez <jm.lopez@utorontoca>
2017-08-15 00:42:07 -0500

    * FeaturedImage or 404

--- iotd/images/views.py
+++ iotd/images/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render_to_response
 from images.models import FeaturedImage
 from django.conf import settings
+from django.http import Http404
 
 def home(request):
-    image = FeaturedImage.objects.latest('uploaded') 
-    print(settings.STATIC_ROOT)
-    return render_to_response('images/home.html',
-                              { 'image' : image})
+    try:
+        image = FeaturedImage.objects.latest('uploaded') 
+        print(settings.STATIC_ROOT)
+        return render_to_response('images/home.html',
+                                  { 'image' : image})
+    except FeaturedImage.DoesNotExist:
+        raise Http404('No %s matches the given query.' % FeaturedImage._meta.object_name)
